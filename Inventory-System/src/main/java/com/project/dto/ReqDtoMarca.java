9346e1b01c2e8ea3d2dd9eece16740ff29d6ebbb
Numi <barbaravalentinabecerra@gmail.com>
2019-12-23 12:59:30 -0300

    garcias

---
+++ Inventory-System/src/main/java/com/project/dto/ReqDtoMarca.java
@@ -0,0 +1,22 @@
+package com.project.dto;
+
+public class ReqDtoMarca {
+    private Long idMarcaDto;
+    private String nombreMarcaDto;
+
+    public Long getIdMarcaDto() {
+        return idMarcaDto;
+    }
+
+    public void setIdMarcaDto(Long idMarcaDto) {
+        this.idMarcaDto = idMarcaDto;
+    }
+
+    public String getNombreMarcaDto() {
+        return nombreMarcaDto;
+    }
+
+    public void setNombreMarcaDto(String nombreMarcaDto) {
+        this.nombreMarcaDto = nombreMarcaDto;
+    }
+}
