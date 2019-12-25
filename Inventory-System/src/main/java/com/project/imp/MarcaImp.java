8d60297a0d114490e0b198976e34092f07318ac3
a7xnumi <barbaravalentinabecerra@gmail.com>
2019-12-25 05:02:05 -0300

    GARCIAS x3

--- Inventory-System/src/main/java/com/project/imp/MarcaImp.java
+++ Inventory-System/src/main/java/com/project/imp/MarcaImp.java
@@ -1,6 +1,30 @@
 package com.project.imp;
 
+import com.project.dto.ReqDtoMarca;
+import com.project.model.Marca;
 import com.project.service.IMarcaService;
 
 public class MarcaImp implements IMarcaService {
+
+    @Override
+    public Marca agregarMarca(ReqDtoMarca reqDtoMarca) throws Exception{
+        try{
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+            throw new Exception();
+        }
+        return null;
+    }
+
+    @Override
+    public Marca eliminarMarca() throws Exception {
+        try{
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+            throw new Exception();
+        }
+        return null;
+    }
 }
--- Inventory-System/src/main/java/com/project/service/IMarcaService.java
+++ Inventory-System/src/main/java/com/project/service/IMarcaService.java
@@ -1,4 +1,9 @@
 package com.project.service;
 
+import com.project.dto.ReqDtoMarca;
+import com.project.model.Marca;
+
 public interface IMarcaService {
+    Marca agregarMarca(ReqDtoMarca reqDtoMarca) throws Exception;
+    Marca eliminarMarca() throws Exception;
 }
