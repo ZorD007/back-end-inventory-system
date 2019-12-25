9b1697f909cc778d1c01ab03cea13455c0ab8202
a7xnumi <barbaravalentinabecerra@gmail.com>
2019-12-25 04:55:33 -0300

    GARCIAS x2

--- Inventory-System/src/main/java/com/project/mapping/MappingObjetoMarca.java
+++ Inventory-System/src/main/java/com/project/mapping/MappingObjetoMarca.java
@@ -1,6 +1,7 @@
 package com.project.mapping;
 
 import com.project.dto.ReqDtoMarca;
+import com.project.dto.ResponseDtoMarca;
 import com.project.model.Marca;
 
 public class MappingObjetoMarca {
@@ -19,17 +20,16 @@ public class MappingObjetoMarca {
         return marcaLocal;
     }
 
-    public Marca transformModelaResponse(ReqDtoMarca reqDtoMarca) throws Exception{
-        Marca marcaLocal;
+    public ResponseDtoMarca transformModelaResponse(Marca marca) throws Exception{
+            ResponseDtoMarca responseDtoMarca;
         try{
-            marcaLocal = new Marca();
-
-
+            responseDtoMarca = new ResponseDtoMarca();
+            responseDtoMarca.setMarcaDto(marca.getNombreMarca());
 
         }catch(Exception ex){
             ex.printStackTrace();
             throw new Exception();
         }
-        return marcaLocal;
+        return responseDtoMarca;
     }
 }
