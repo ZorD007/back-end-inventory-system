8053d0e43d546995ca1f2fd5a04cf152de0b92eb
KleinLohgock <klein_lohgock@hotmail.com>
2020-01-15 14:57:15 -0300

    Rol Imp lolxd

--- Inventory-System/src/main/java/com/project/imp/RolImp.java
+++ Inventory-System/src/main/java/com/project/imp/RolImp.java
@@ -1,16 +1,23 @@
 package com.project.imp;
 
+import com.project.dto.ReqDtoProducto;
 import com.project.dto.ReqDtoRol;
+import com.project.dto.ResponseDtoProducto;
 import com.project.dto.ResponseDtoRol;
+import com.project.exception.NoActualizarException;
 import com.project.exception.NoEncontradoException;
 import com.project.exception.NoGuardadoException;
 import com.project.mapping.MappingObjetoRol;
+import com.project.model.Producto;
 import com.project.model.Rol;
 import com.project.repository.RolRepository;
 import com.project.service.IRolService;
 import com.project.util.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RolImp implements IRolService {
@@ -45,6 +52,35 @@ public class RolImp implements IRolService {
     }
 
     @Override
+    public ResponseDtoRol modificarRol(Long id, ReqDtoRol reqDtoRol) throws Exception {
+        ResponseDtoRol responseDtoRol= null;
+        try{
+
+            Rol rol = rolRepository.findById(id).get();
+            if(null != reqDtoRol){
+
+                rol.setCargo(reqDtoRol.getCargoDto());
+                rol.setIdRol(reqDtoRol.getIdRolDto());
+
+                Rol rolActualizado = rolRepository.save(rol);
+                responseDtoRol = mappingObjetoRol.transformModelaResponse(rolActualizado);
+
+                rol.setCargo(reqDtoRol.getCargoDto());
+                rol.setIdRol(reqDtoRol.getIdRolDto());
+
+                responseDtoRol = mappingObjetoRol.transformModelaResponse(rolRepository.saveAndFlush(rol));
+
+            }else{
+                throw new NoActualizarException(Constant.ERROR_ACTUALIZAR);
+            }
+        }catch(Exception ex){
+
+            ex.printStackTrace();
+        }
+        return responseDtoRol;
+    }
+
+    @Override
     public Rol buscarPorId(Long id) throws Exception {
         Rol rolLocal;
         try{
@@ -59,5 +95,41 @@ public class RolImp implements IRolService {
             throw new Exception(Constant.ERROR_SISTEMA);
         }
         return rolLocal;
+    }
+
+    @Override
+    public boolean eliminarRol(Long id) throws Exception {
+        try{
+            Rol rol = mappingObjetoRol.transformOptionalaModel(rolRepository.findById(id));
+            if(null == rol){
+                throw new NoEncontradoException(Constant.ERROR_NO_ENCONTRADO);
+            }else{
+                rolRepository.deleteById(id);
+                return true;
+            }
+        }catch (NoEncontradoException ex){
+            ex.printStackTrace();
+            throw new NoEncontradoException(ex.getMessage());
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new Exception(Constant.ERROR_SISTEMA);
+        }
+    }
+
+    @Override
+    public List<ResponseDtoRol> listarRol() throws Exception {
+
+        List<ResponseDtoRol> listRol = new ArrayList<>();
+        try {
+            List<Rol> rols = rolRepository.findAll();
+            for(Rol p : rols){
+                listRol.add(mappingObjetoRol.transformModelaResponse( p ));
+            }
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new Exception(Constant.ERROR_SISTEMA);
+        }
+        return listRol;
     }
 }
